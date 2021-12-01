2a2a206c8c718515f8bf9c894a13ebdab7f8a6a5
Artem <kumikozp123@gmail.com>
2021-12-02 01:16:09 +0600

    minor form styles improvment

--- src/js/App.js
+++ src/js/App.js
@@ -7,11 +7,10 @@ import * as Yup from "yup";
 export default function Form(){
 
    const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
-   let success = "";
    const formik = useFormik({
       initialValues:{
          name: "",
-         cell: ""
+         number: ""
       },
       validationSchema: Yup.object({
          name: Yup.string()
@@ -19,18 +18,22 @@ export default function Form(){
          .max(50, "Поле ФИО должно содержать не более 50 символов")
          .min(3, "Поле ФИО должно содержать минимум 3 символа")
          .required("Поле ФИО обязательно для заполнения"),
-         cell: Yup.string()
+         number: Yup.string()
          .matches(phoneRegExp, 'Не правильный номер телефона')
          .max(15, "Телефон не может содержать больше 15 символов")
          .required("Поле телефон обязательно для заполнения")
       }),
       onSubmit: (values)=>{
          
+         const resultEl = document.getElementsByClassName("form__success")[0];
+
          emailjs.sendForm('service_yjvlwin', 'template_hwzldie', document.getElementsByClassName("form")[0], 'user_IPHVGFLVcq9Zskg2vOUJi')
          .then((result) => {
-            document.getElementsByClassName("form__success")[0].innerHTML = "Данные успешно отправлены!";
+            resultEl.innerHTML = "Данные успешно отправлены";
+            resultEl.classList.add("form__success-show");
          }, (error) => {
-            document.getElementsByClassName("form__success")[0].innerHTML = "Ошибка. Что-то пошло не так.";
+            resultEl.innerHTML = "Ошибка. Данные не были отправлены";
+            resultEl.classList.add("form__success-show");
          });
          
       }
@@ -41,20 +44,23 @@ export default function Form(){
                <form onSubmit={formik.handleSubmit} className="form">
                   <div className="form__item">
                      <label className="form__label">
-                        ФИО
-                        <br/>
+                        <div className="form__label-container">
+                           ФИО
+                           <p className="form__error">{formik.errors.name}</p>
+                        </div>
                         <input type="text" className="form__textbox" name="name"
                         value={formik.values.name} onChange={formik.handleChange}/>
                      </label>
-                     <h3 className="form__error">{formik.errors.name}</h3>
                   </div>
                   <div className="form__item">
-                     <label className="form__label">Телефон
-                        <br/>
-                        <input type="text" className="form__textbox" name="cell"
-                        value={formik.values.cell} onChange={formik.handleChange}/>
+                     <label className="form__label">
+                     <div className="form__label-container">
+                           Телефон
+                           <p className="form__error">{formik.errors.number}</p>
+                        </div>
+                        <input type="text" className="form__textbox" name="number"
+                        value={formik.values.number} onChange={formik.handleChange}/>
                      </label>
-                     <h3 className="form__error">{formik.errors.cell}</h3>
                   </div>
                   <div className="form__footer">
                         <input type="submit" name="sumbit" value="Отправить" className="btn_short"/>
@@ -63,7 +69,7 @@ export default function Form(){
                         <a href="#" className="form__policy-link"> политикой конфиденциальности</a> 
                      </p>
                   </div>
-                  <p className="form__success">{success}</p>
+                  <p className="form__success">Данные успешно отправлены</p>
                </form>
             </div>
       );  
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-var path = require('path');module.exports = {
+var path = require('path');
+module.exports = {
     entry: './src/js/script.js',
     mode: "development",
     target: "node", 
